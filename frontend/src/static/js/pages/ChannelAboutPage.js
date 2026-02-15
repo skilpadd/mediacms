a574d326c9083ddd67fabcbf970e23f05e985e77
Dmitry Zolotukhin <zolotuhin.dmitriy@mail.ru>
2026-02-15 15:00:41 +0300

    feat(ProfilePage.scss, ChannelAboutPage): add displaying tags and categories for channels

--- frontend/src/static/js/pages/ChannelAboutPage.js
+++ frontend/src/static/js/pages/ChannelAboutPage.js
@@ -5,6 +5,31 @@ import ProfilePagesContent from '../components/profile-page/ProfilePagesContent'
 import { MediaListRow } from '../components/MediaListRow';
 import { ChannelMediaPage } from './ChannelMediaPage';
 import ChannelPagesHeader from '../components/channel-page/ChannelPagesHeader';
+import { translateString } from '../utils/helpers';
+
+function MetaField(props) {
+  return (
+    <div className={props.id ? 'media-content-' + props.id : null}>
+      <div className="media-content-field">
+        <div className="media-content-field-label">
+          <h4>{props.title}</h4>
+        </div>
+        <div className="media-content-field-content">{props.value}</div>
+      </div>
+    </div>
+  );
+}
+
+function metaLinks(arr) {
+  if (!arr | !arr.length) return [];
+  const sep = arr.length > 1 ? ', ' : '';
+  return arr.map((item, i) => (
+    <div key={i}>
+      <a href={item.url} title={item.title}>{item.title}</a>
+      {i < arr.length - 1 ? sep : ''}
+    </div>
+  ));
+}
 
 export class ChannelAboutPage extends ChannelMediaPage {
   constructor(props) {
@@ -25,6 +50,9 @@ export class ChannelAboutPage extends ChannelMediaPage {
     const siteUrl = SiteContext._currentValue.url.replace(/\/+$/, '');
     const channelUrl = siteUrl + '/channel/' + this.state.channelToken;
 
+    const tags = metaLinks(channelData.tags_info);
+    const categories = metaLinks(channelData.categories_info);
+
     return [
       <ChannelPagesHeader
         key="ChannelPagesHeader"
@@ -34,8 +62,26 @@ export class ChannelAboutPage extends ChannelMediaPage {
       />,
       <ProfilePagesContent key="ProfilePagesContent">
         <div className="media-list-wrapper items-list-ver profile-about-content">
-          <MediaListRow title="About">
-            {channelData.description || "No description available"}
+          <MediaListRow title={translateString("About")}>
+            <div style={{ marginBottom: '24px'}}>
+              {channelData.description || translateString("No description available")}
+            </div>
+
+            {tags.length ? (
+              <MetaField
+                value={tags}
+                title={tags.length > 1 ? translateString("Tags") : translateString("Tag")}
+                id="tags"
+              />
+            ): null}
+
+            {categories.length ? (
+              <MetaField
+                value={categories}
+                title={categories.length > 1 ? translateString("Categories") : translateString("Category")}
+                id="categories"
+              />
+            ) : null}
           </MediaListRow>
         </div>
       </ProfilePagesContent>
