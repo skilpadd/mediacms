87933045dbade21aa0011fb9f36b88f5b91a7fdb
Dmitry Zolotukhin <zolotuhin.dmitriy@mail.ru>
2026-02-15 20:20:27 +0300

    feat(cms/settings.py, files/migrations, files/models/media.py, edit_media.html): add slug support for media

--- files/models/media.py
+++ files/models/media.py
@@ -17,6 +17,7 @@ from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.html import strip_tags
+from django.utils.text import slugify
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFit
 
@@ -70,7 +71,7 @@ class Media(models.Model):
         help_text="Whether media is globally featured by a MediaCMS editor",
     )
 
-    friendly_token = models.CharField(blank=True, max_length=150, db_index=True, unique=True, help_text="Identifier for the Media")
+    friendly_token = models.SlugField(blank=True, max_length=150, db_index=True, unique=True, help_text="Identifier for the Media")
 
     hls_file = models.CharField(max_length=1000, blank=True, help_text="Path to HLS file for videos")
 
@@ -255,12 +256,26 @@ class Media(models.Model):
             self.add_date = timezone.now()
 
         if not self.friendly_token:
-            # get a unique identifier
-            while True:
-                friendly_token = helpers.produce_friendly_token()
-                if not Media.objects.filter(friendly_token=friendly_token):
-                    self.friendly_token = friendly_token
-                    break
+            if self.title:
+                slugged_title = slugify(self.title)
+                slug = slugged_title
+                counter = 1
+
+                query_slug = Media.objects.all()
+                if self.pk:
+                    query_slug = query_slug.exclude(pk=self.pk)
+                while query_slug.filter(friendly_token=slug).exists():
+                    slug = f'{slugged_title}-{counter}'
+                    counter += 1
+
+                self.friendly_token = slug
+
+            if not self.friendly_token:
+                while True:
+                    friendly_token = helpers.produce_friendly_token()
+                    if not Media.objects.filter(friendly_token=friendly_token):
+                        self.friendly_token = friendly_token
+                        break
 
         if self.pk:
             # media exists
