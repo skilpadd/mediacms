703317ea7d864a39a835fac13760f836eb9b5e66
Dmitry Zolotukhin <zolotuhin.dmitriy@mail.ru>
2026-02-21 09:54:34 +0300

    feat(static/images, cms/settings.py): update logo

--- cms/settings.py
+++ cms/settings.py
@@ -117,9 +117,9 @@ SITE_ID = 1
 # PORTAL_LOGO_DARK_SVG = ""
 # PORTAL_LOGO_LIGHT_SVG = ""
 # place the files on static/images folder
-PORTAL_LOGO_DARK_SVG = "/static/images/logo_dark.svg"
+PORTAL_LOGO_DARK_SVG = ""
 PORTAL_LOGO_DARK_PNG = "/static/images/logo_dark.png"
-PORTAL_LOGO_LIGHT_SVG = "/static/images/logo_light.svg"
+PORTAL_LOGO_LIGHT_SVG = ""
 PORTAL_LOGO_LIGHT_PNG = "/static/images/logo_dark.png"
 
 # paths to extra css files to be included, eg "/static/css/custom.css"
