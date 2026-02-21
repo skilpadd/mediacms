dd0ea83c7b59ac38168b2da7b0421aa02d23149b
Dmitry Zolotukhin <zolotuhin.dmitriy@mail.ru>
2026-02-21 10:28:36 +0300

    feat(settings.py, frontend/src/static/images): update logo icons

--- cms/settings.py
+++ cms/settings.py
@@ -120,7 +120,7 @@ SITE_ID = 1
 PORTAL_LOGO_DARK_SVG = ""
 PORTAL_LOGO_DARK_PNG = "/static/images/logo_dark.png"
 PORTAL_LOGO_LIGHT_SVG = ""
-PORTAL_LOGO_LIGHT_PNG = "/static/images/logo_dark.png"
+PORTAL_LOGO_LIGHT_PNG = "/static/images/logo_light.png"
 
 # paths to extra css files to be included, eg "/static/css/custom.css"
 # place css inside static/css folder
