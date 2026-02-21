37add6838ed9b0fa32dc8ae4512870d4cb52349c
Dmitry Zolotukhin <zolotuhin.dmitriy@mail.ru>
2026-02-21 15:01:59 +0300

    feat(frontentd/src/static): update favicon icons

--- frontend/config/mediacms.config.html.js
+++ frontend/config/mediacms.config.html.js
@@ -19,7 +19,6 @@ module.exports = {
       { rel: 'apple-touch-icon', sizes: '180x180', href: 'static/favicons/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'static/favicons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'static/favicons/favicon-16x16.png' },
-      { rel: 'mask-icon', href: 'static/favicons/safari-pinned-tab.svg', color: '#fafafa' },
       { rel: 'shortcut icon', href: 'static/favicons/favicon.ico' },
       /**
        * Stylesheet links
